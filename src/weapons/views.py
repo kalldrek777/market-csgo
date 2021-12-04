c64f4e7e78261e4c90e8c6b4b51013ccfb453397
Market <kalldrek@yandex.ru>
2021-12-04 16:00:41 +0300

    Поправки

--- src/weapons/views.py
+++ src/weapons/views.py
@@ -13,6 +13,15 @@ def knife(request):
     qs = Product.objects.all().order_by('-date')
     paginator = Paginator(qs, 8)  # Show 8 contacts per page.
 
+    form = SearchForm()
+    if request.method == 'POST':
+        form = SearchForm(request.POST)
+        if form.is_valid():
+            form_query = form.cleaned_data
+            #   print(form_query['query'])
+            query = form_query['query']
+            return render(request, 'Search.html', {'qs': qs, 'form': form, 'query': query})
+
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
 
