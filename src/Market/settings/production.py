2f27b193792031d291b7af8019208b13f318d208
Market <kalldrek@yandex.ru>
2021-12-07 13:22:57 +0300

    Поправки

--- src/Market/settings/production.py
+++ src/Market/settings/production.py
@@ -136,9 +136,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_ROOT = BASE_DIR.joinpath('staticfiles')
 
-STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_URL = '/media/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
