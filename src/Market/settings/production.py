3584c295b3f17954b6b612df3359f16e5a5b33f4
Market <kalldrek@yandex.ru>
2021-12-07 14:17:04 +0300

    Поправки

--- src/Market/settings/production.py
+++ src/Market/settings/production.py
@@ -136,17 +136,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_ROOT = BASE_DIR.joinpath('staticfiles')
+# STATIC_ROOT = BASE_DIR.joinpath('staticfiles')
 
-#STATIC_ROOT = 'staticfiles'
-#STATIC_ROOT = os.path.join(BASE_DIR, 'media/')
+# STATIC_ROOT = 'staticfiles'
+STATIC_ROOT = BASE_DIR / 'static'
 STATIC_URL = '/static/'
-# STATICFILES_DIRS = (
-#     os.path.join(BASE_DIR, 'media/'),
-# )
+STATIC_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+# MEDIA_URL = '/static/'
+# MEDIA_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
