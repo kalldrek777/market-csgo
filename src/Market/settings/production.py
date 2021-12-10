012276e4123b6902c6d6cc86eb1f74d67f227751
Market <kalldrek@yandex.ru>
2021-12-10 14:13:53 +0300

    Поправки

--- src/Market/settings/production.py
+++ src/Market/settings/production.py
@@ -138,9 +138,10 @@ USE_TZ = True
 
 STATIC_ROOT = BASE_DIR.joinpath('staticfiles')
 STATIC_URL = '/static/'
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
